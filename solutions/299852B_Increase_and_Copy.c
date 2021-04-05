b8d03dcffb95f331221625abbc084617c70db0b5
rheallyc <vireonxi@gmail.com>
2021-04-05 03:52:21 +0000

    Solve 299852B: Increase and Copy

---
+++ solutions/299852B_Increase_and_Copy.c
@@ -0,0 +1,48 @@
+// Problem: Increase and Copy
+// URL: https://codeforces.com/gym/299852/problem/B
+// Rating: 1100
+// Tags: none (gym/team contest)
+// Language: GNU C11
+#include <stdio.h>
+#include <math.h>
+void solve(void);
+
+void solve(void)
+{
+    int n, i=2, j, step;
+    scanf("%d",&n);
+    step=n-1;
+    if (n<1000)
+    {
+       for(;i<=n;i++)
+       {
+           j=n/i;
+           if(n%i==0)
+            j--;
+           if(step>i+j-1)
+            step=i+j-1;
+       }
+    }
+    else
+    {
+        for (i=sqrt(n)-10;i<=sqrt(n)+10;i++)
+        {
+            j=n/i;
+            if(n%i==0)
+             j--;
+            if(step>i+j-1)
+             step=i+j-1;
+        }
+    }
+    printf("%d\n", step);
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
