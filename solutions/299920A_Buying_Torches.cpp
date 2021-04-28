dbe10382636b99ac0ef12ad67751205eb07e4041
rheallyc <vireonxi@gmail.com>
2021-04-28 06:20:21 +0000

    Solve 299920A: Buying Torches

---
+++ solutions/299920A_Buying_Torches.cpp
@@ -0,0 +1,24 @@
+// Problem: Buying Torches
+// URL: https://codeforces.com/gym/299920/problem/A
+// Rating: 1000
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <stdio.h>
+int x, y;
+long long i, k;
+
+void solve(void)
+{
+    scanf("%d %d %lld", &x, &y, &k);
+    i=(long long)(k*y+k+x-3)/(x-1);
+    printf("%lld\n", i+k);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
