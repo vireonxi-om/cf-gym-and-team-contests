51bcd9a6f17e3ad8b281f0aac9f35615982fdcf4
rheallyc <vireonxi@gmail.com>
2021-06-30 23:28:21 +0000

    Solve 102556E: Riana's Excruciating Enhancement Enigma

---
+++ solutions/102556E_Rianas_Excruciating_Enhancement_Enigma.cpp
@@ -0,0 +1,16 @@
+// Problem: Riana's Excruciating Enhancement Enigma
+// URL: https://codeforces.com/gym/102556/problem/E
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, m;
+
+int main(){
+    scanf("%lld", &n);
+    printf("%lld\n", n);
+    return 0;
+}
+
