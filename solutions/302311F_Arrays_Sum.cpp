fe182aa13dfa293dd6f28a065980428fc8fa3afa
rheallyc <vireonxi@gmail.com>
2021-07-08 00:53:08 +0000

    Solve 302311F: Arrays Sum

---
+++ solutions/302311F_Arrays_Sum.cpp
@@ -0,0 +1,37 @@
+// Problem: Arrays Sum
+// URL: https://codeforces.com/gym/302311/problem/F
+// Rating: 1400
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, total, now, num;
+
+void solve()
+{
+	scanf("%d%d", &n, &k); 
+	total=0; now=-1;
+	for(int i=1; i<=n; i++){
+		scanf("%d", &num);
+		if(num!=now){
+			now=num; total++;
+		}
+	}
+	if(k==1){
+		if(total==1) printf("%d\n", 1);
+		else printf("%d\n", -1);
+	} 
+	else{
+		if(total==1) printf("%d\n", 1);
+		else printf("%d\n", (total-2)/(k-1)+1);
+	}
+	
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for(int i=0; i<T; i++) solve();
+	return 0;
+} 
