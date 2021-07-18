c76c1ca8166af7f9a744830449fefa9fb09ea749
rheallyc <vireonxi@gmail.com>
2021-07-18 05:02:35 +0000

    Solve 306200D: Power Sequence

---
+++ solutions/306200D_Power_Sequence.cpp
@@ -0,0 +1,32 @@
+// Problem: Power Sequence
+// URL: https://codeforces.com/gym/306200/problem/D
+// Rating: 1500
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[110000];
+
+int main(){
+    scanf("%d", &n);
+    ll sum=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        sum+=a[i];
+    }
+    sort(a+1, a+n+1);
+    ll result=sum-n;
+    for(int i=1; ;i++){
+        ll now=1, tot=0;
+        for(int j=1; j<=n; j++){
+            tot+=abs(now-a[j]);
+            if(tot>result) break;
+            now*=i;
+        }
+        if(tot>result) break;
+        result=tot;
+    }
+    printf("%lld\n", result);
+    return 0;
+}
