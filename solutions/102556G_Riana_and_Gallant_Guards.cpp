68577d96b67e52b7d6da576d75bbc1669c719d05
rheallyc <vireonxi@gmail.com>
2021-07-30 04:00:33 +0000

    Solve 102556G: Riana and Gallant Guards

---
+++ solutions/102556G_Riana_and_Gallant_Guards.cpp
@@ -0,0 +1,19 @@
+// Problem: Riana and Gallant Guards
+// URL: https://codeforces.com/gym/102556/problem/G
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m, x, y;
+
+int main(){
+    scanf("%d%d%d%d", &n, &m, &x, &y);
+    int a=min(x, n-x+1), b=min(y, m-y+1);
+    int tot=m*n, ans=a*b;
+    if(tot-ans<=ans) printf("I don't wanna do this anymore!\n");
+    else printf("%d\n", ans);
+    return 0;
+}
+
