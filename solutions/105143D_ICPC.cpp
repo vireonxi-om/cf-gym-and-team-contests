8d417c221c71faa782f959060b57a6f4e9cfa76e
rheallyc <vireonxi@gmail.com>
2021-08-02 20:21:37 +0000

    Solve 105143D: ICPC

---
+++ solutions/105143D_ICPC.cpp
@@ -0,0 +1,82 @@
+// Problem: ICPC
+// URL: https://codeforces.com/gym/105143/problem/D
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++20 (GCC 13-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 5100;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n, x;
+LL a[N], s[N], mx1[N + N + N], mx2[N + N + N], dp[N][N + N];
+int main() {
+    IO;
+    cin >> n;
+    repn(i, 1, n) cin >> a[i], s[i] = s[i - 1] + a[i];
+    pern(i, 1, n) {
+        repn(j, 0, n + n) {
+            LL v = a[i] + max(s[i - 1] - s[max(i - 1 - j, 0)], s[min(i + j, n)] - s[i]);
+            mx1[i + j] = max(mx1[i + j], v);
+        }
+        repn(j, 1, n + n) {
+            dp[i][j] = max(dp[i][j], mx1[i + j]);
+        }
+    }
+    repn(i, 1, n) {
+        repn(j, 0, n + n) {
+            LL v = a[i] + max(s[i - 1] - s[max(i - 1 - j, 0)], s[min(i + j, n)] - s[i]);
+            mx2[j - i + N] = max(mx2[j - i + N], v);
+        }
+        repn(j, 1, n + n) {
+            dp[i][j] = max(dp[i][j], mx2[j - i + N]);
+        }
+    }
+    LL ans = 0;
+    repn(i, 1, n) {
+        LL res = 0;
+        repn(j, 1, n + n) {
+            LL v = dp[i][j];
+            // cout << i << " " << j << " " << v << "\n";
+            res ^= j * v;
+        } 
+        ans ^= i + res;
+    }
+    cout << ans << "\n";
+    return 0;
+}
