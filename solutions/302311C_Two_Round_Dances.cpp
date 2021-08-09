d299ade1ddb7e78c28081fdb91b28aa9fb6303ba
rheallyc <vireonxi@gmail.com>
2021-08-09 13:47:17 +0000

    Solve 302311C: Two Round Dances

---
+++ solutions/302311C_Two_Round_Dances.cpp
@@ -0,0 +1,16 @@
+// Problem: Two Round Dances
+// URL: https://codeforces.com/gym/302311/problem/C
+// Rating: 1300
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+long long n, result=1;
+int main()
+{
+	scanf("%lld", &n);
+	for(int i=1; i<n; i++) result*=i;
+	result/=(n/2);
+	printf("%lld", result);
+	return 0;
+}
