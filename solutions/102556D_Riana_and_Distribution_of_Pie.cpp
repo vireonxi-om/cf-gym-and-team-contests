7a0447afd029aee6390628ebd73f1650be508d07
rheallyc <vireonxi@gmail.com>
2021-09-09 18:27:26 +0000

    Solve 102556D: Riana and Distribution of Pie

---
+++ solutions/102556D_Riana_and_Distribution_of_Pie.cpp
@@ -0,0 +1,19 @@
+// Problem: Riana and Distribution of Pie
+// URL: https://codeforces.com/gym/102556/problem/D
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        printf("%.6f ", (double)100/i);
+    }
+    printf("\n");
+    return 0;
+}
+
