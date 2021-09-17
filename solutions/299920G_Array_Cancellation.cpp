f505390cd9204a7662cf4ea07344d1d769c58d06
rheallyc <vireonxi@gmail.com>
2021-09-17 08:43:04 +0000

    Solve 299920G: Array Cancellation

---
+++ solutions/299920G_Array_Cancellation.cpp
@@ -0,0 +1,36 @@
+// Problem: Array Cancellation
+// URL: https://codeforces.com/gym/299920/problem/G
+// Rating: 1000
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <stdio.h>
+#include <string.h>
+long long a[110000], sum;
+int n;
+
+void solve(void)
+{
+    memset(a, 0, 110000); sum=0;
+    scanf("%d", &n);
+    for (int i=1; i<=n-1; i++)
+    {
+        scanf("%lld", &a[i]);
+        if(a[i]>=0)
+            sum+=a[i];
+        else if(sum+a[i]>=0)
+            sum+=a[i];
+        else
+            sum=0;
+    }
+    scanf("%lld", &a[n]);
+    printf("%lld\n", sum+a[n]);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
