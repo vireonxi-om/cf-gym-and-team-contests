6380436fe5627571da41780ee53eae6ecc40eaf7
rheallyc <vireonxi@gmail.com>
2021-12-05 16:33:10 +0000

    Solve 309196C: p-binary

---
+++ solutions/309196C_p-binary.cpp
@@ -0,0 +1,36 @@
+// Problem: p-binary
+// URL: https://codeforces.com/gym/309196/problem/C
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, p;
+
+int cnt(ll num){
+    int result=0;
+    while(num>0){
+        if(num%2){
+            num--; result++;
+        }
+        num/=2;
+    }
+    return result;
+}
+
+int main(){
+    scanf("%lld%lld", &n, &p);
+    bool can=false;
+    ll ans=n; int result;
+    for(int i=1; i<=31; i++){
+        ans-=p;
+        if(ans<=0) break;
+        if(cnt(ans)<=i && ans>=i){
+            can=true; result=i; break;
+        }
+    }
+    if(can) printf("%d\n", result);
+    else printf("-1\n");
+    return 0;
+}
