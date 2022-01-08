a3062434583d8c944ed73067eae82791c09a7dab
rheallyc <vireonxi@gmail.com>
2022-01-08 08:30:32 +0000

    Solve 302720B: Palindromifier

---
+++ solutions/302720B_Palindromifier.cpp
@@ -0,0 +1,16 @@
+// Problem: Palindromifier
+// URL: https://codeforces.com/gym/302720/problem/B
+// Rating: 1400
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    char s[110000];
+    scanf("%s", &s);
+    printf("%d\n", 3);
+    printf("L %d\n", 2);
+    printf("R %d\n", 2);
+    printf("R %d\n", 2*strlen(s)-1);
+    return 0;
+}
