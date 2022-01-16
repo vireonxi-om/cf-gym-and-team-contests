ea52fe5803f8d98a8ae725be87ad5c24e6d72c82
rheallyc <vireonxi@gmail.com>
2022-01-16 02:42:16 +0000

    Solve 323731E: Really Big Numbers

---
+++ solutions/323731E_Really_Big_Numbers.cpp
@@ -0,0 +1,37 @@
+// Problem: Really Big Numbers
+// URL: https://codeforces.com/gym/323731/problem/E
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ll n, s;
+
+ll func(ll a){
+    ll result=0;
+    while(a){
+        result+=a%10;
+        a/=10;
+    }
+    return result;
+}
+
+int main(){
+    scanf("%lld%lld", &n, &s);
+    ll r=n, l=s;
+    ll ans=-1;
+    while(l<r){
+        ll mid=(l+r)/2;
+        if(mid-func(mid)>=s){
+            ans=mid;
+            r=mid;
+        }
+        else l=mid+1;
+    }
+    if(l-func(l)>=s) ans=l;
+    if(ans==-1) ans=0;
+    else ans=n-ans+1;
+    printf("%lld\n", ans);
+    return 0;
+}
