f72847abd37d4ab138746d14e959b82dcc659dc2
rheallyc <vireonxi@gmail.com>
2022-02-08 07:05:43 +0000

    Solve 314705A: Permutation Game

---
+++ solutions/314705A_Permutation_Game.cpp
@@ -0,0 +1,29 @@
+// Problem: Permutation Game
+// URL: https://codeforces.com/gym/314705/problem/A
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, a[110000], pos[110000];
+char s[110000];
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        pos[a[i]]=i;
+    }
+    s[pos[n]]='B';
+    for(int t=n-1; t>=1; t--){
+        int st=pos[t]%t;
+        s[pos[t]]='B';
+        for(int i=st; i<=n; i+=t){
+            if(i==pos[t]) continue;
+            if(a[i]>t && s[i]=='B') s[pos[t]]='A';
+        }
+    }
+    printf("%s", s+1);
+    return 0;
+}
