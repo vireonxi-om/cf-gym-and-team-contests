b1db483fcf1ef3cf418e3d80f7bc9989b6337f01
rheallyc <vireonxi@gmail.com>
2022-03-08 11:42:19 +0000

    Solve 299852D: Floor Number

---
+++ solutions/299852D_Floor_Number.c
@@ -0,0 +1,27 @@
+// Problem: Floor Number
+// URL: https://codeforces.com/gym/299852/problem/D
+// Rating: 800
+// Tags: none (gym/team contest)
+// Language: GNU C11
+#include <stdio.h>
+void solve(void);
+
+void solve(void)
+{
+    int n, x, count=0;
+    scanf("%d%d",&n,&x);
+    for(;n>2;n-=x,count++);
+    printf("%d\n",count+1);
+}
+
+
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
