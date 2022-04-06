4913ebae7169dd7a1f1ea584fe3cdb6904f9ea4e
rheallyc <vireonxi@gmail.com>
2022-04-06 17:43:29 +0000

    Solve 312858A: Brick

---
+++ solutions/312858A_Brick.cpp
@@ -0,0 +1,15 @@
+// Problem: Brick
+// URL: https://codeforces.com/gym/312858/problem/A
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, w;
+
+int main(){
+    scanf("%d%d", &n, &w);
+    printf("%d\n", n/w);
+    return 0;
+}
