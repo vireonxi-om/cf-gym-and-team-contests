caf8102ce6dbfb3c317935db3b50e7deb49de4db
rheallyc <vireonxi@gmail.com>
2022-05-09 06:16:27 +0000

    Solve 307718B: Celex Update

---
+++ solutions/307718B_Celex_Update.cpp
@@ -0,0 +1,26 @@
+// Problem: Celex Update
+// URL: https://codeforces.com/gym/307718/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a1, b1, a2, b2;
+ll result;
+
+void solve(){
+    scanf("%d%d%d%d", &a1, &b1, &a2, &b2);
+    result=1;
+    result*=(a2-a1);
+    result*=(b2-b1);
+    result++;
+    printf("%lld\n", result);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}
