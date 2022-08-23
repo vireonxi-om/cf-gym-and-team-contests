7f311095602d3aa8bfb7bdee03379d5867d5f237
rheallyc <vireonxi@gmail.com>
2022-08-23 17:25:50 +0000

    Solve 104090D: Money Game

---
+++ solutions/104090D_Money_Game.cpp
@@ -0,0 +1,59 @@
+// Problem: Money Game
+// URL: https://codeforces.com/gym/104090/problem/D
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n;
+long double a[N], sum; 
+int main() {
+	IO;
+	cin >> n;
+	repn(i, 1, n) cin >> a[i], sum += a[i];
+	sum /= (n + 1);
+	repn(i, 1, n) {
+		long double x = sum + sum * (i == 1);
+		printf("%.10f", (double)x);
+		if (i != n) printf(" ");
+		else printf("\n");
+	}
+	return 0;
+}
