1a196a0e1dcf7a6e3ad5b1fa53220c8413023f6a
rheallyc <vireonxi@gmail.com>
2022-09-19 21:52:27 +0000

    Solve 328370A: New Year and Rating

---
+++ solutions/328370A_New_Year_and_Rating.cpp
@@ -0,0 +1,29 @@
+// Problem: New Year and Rating
+// URL: https://codeforces.com/gym/328370/problem/A
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+int main(){
+    scanf("%d", &n);
+    int c, d, sum=0;
+    int pmin=-2e9, pmax=2e9;
+    bool can=true;
+    for(int i=1; i<=n; i++){
+        scanf("%d%d", &c, &d);
+        if(d==1) pmin=max(pmin, 1900-sum);
+        else pmax=min(pmax, 1899-sum);
+        sum+=c;
+        if(pmin>pmax) can=false;
+    }
+    if(can){
+        if(pmax>1e9) printf("Infinity\n");
+        else printf("%d\n", pmax+sum);
+    }
+    else printf("Impossible\n");
+    return 0;
+}
