dc246640984cdfbb47282ea6b93383521449e1d8
rheallyc <vireonxi@gmail.com>
2022-10-01 18:09:10 +0000

    Solve 312858C: Unlucky 7

---
+++ solutions/312858C_Unlucky_7.cpp
@@ -0,0 +1,32 @@
+// Problem: Unlucky 7
+// URL: https://codeforces.com/gym/312858/problem/C
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+
+int main(){
+    scanf("%d", &n);
+    int cnt=0;
+    for(int i=1; i<=n; i++){
+        bool o=false, d=false;
+        int now=i;
+        while(now>0){
+            int num=now%10;
+            if(num==7) d=true;
+            now/=10;
+        }
+        now=i;
+        while(now>0){
+            int num=now%8;
+            if(num==7) o=true;
+            now/=8;
+        }
+        if(!o && !d) cnt++;
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
