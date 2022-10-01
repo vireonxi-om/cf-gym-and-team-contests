6f44f90c5b3b4eb342de16037d94463b82a26413
rheallyc <vireonxi@gmail.com>
2022-10-01 07:35:55 +0000

    Solve 322393E: Multi-judge Solving

---
+++ solutions/322393E_Multi-judge_Solving.cpp
@@ -0,0 +1,31 @@
+// Problem: Multi-judge Solving
+// URL: https://codeforces.com/gym/322393/problem/E
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+ll k, a[1100];
+
+int main(){
+    scanf("%d %lld", &n, &k);
+    for(int i=1; i<=n; i++){
+        scanf("%lld", &a[i]);
+    }
+    sort(a+1, a+n+1);
+    int ans=0;
+    for(int i=1; i<=n; i++){
+        if(a[i]<=2*k){ k=max(k, a[i]); continue;}
+        else{
+            while(k*2<a[i]){
+                k*=2;
+                ans++;
+            }
+            k=max(k, a[i]);
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
