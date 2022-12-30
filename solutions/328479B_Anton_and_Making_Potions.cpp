8b26c1b8dc13a052564db2116e29df4fce1ae915
rheallyc <vireonxi@gmail.com>
2022-12-30 00:23:17 +0000

    Solve 328479B: Anton and Making Potions

---
+++ solutions/328479B_Anton_and_Making_Potions.cpp
@@ -0,0 +1,31 @@
+// Problem: Anton and Making Potions
+// URL: https://codeforces.com/gym/328479/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int m, k;
+ll n, x, s, fa[210000], fb[210000], sc[210000], sd[210000];
+
+
+int main(){
+    scanf("%lld%d%d%lld%lld", &n, &m, &k, &x, &s);
+    for(int i=1; i<=m; i++) scanf("%lld", &fa[i]);
+    for(int i=1; i<=m; i++) scanf("%lld", &fb[i]);
+    for(int i=1; i<=k; i++) scanf("%lld", &sc[i]);
+    for(int i=1; i<=k; i++) scanf("%lld", &sd[i]);
+    ll ans=x*n;
+    fa[0]=x;
+    for(int i=0; i<=m; i++){
+        if(fb[i]>s) continue;
+        ll num=n;
+        auto it=upper_bound(sd+1, sd+k+1, s-fb[i]);
+        if(it>sd+1){ it--; num-=sc[it-sd];}
+        ans=min(ans, fa[i]*num);
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
+
