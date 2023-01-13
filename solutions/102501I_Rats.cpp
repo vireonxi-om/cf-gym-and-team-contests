4e5abf793fcf0cae9691c4b2a887528d92f16c8c
rheallyc <vireonxi@gmail.com>
2023-01-13 06:15:30 +0000

    Solve 102501I: Rats

---
+++ solutions/102501I_Rats.cpp
@@ -0,0 +1,37 @@
+// Problem: Rats
+// URL: https://codeforces.com/gym/102501/problem/I
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+
+LL n1, n2, n12;
+
+int main() {
+	IO;
+	cin >> n1 >> n2 >> n12;
+	LL ans = (n1 + 1) * (n2 + 1) / (n12 + 1);
+	ans -= 1;
+	cout << ans << endl;
+	return 0;
+}
