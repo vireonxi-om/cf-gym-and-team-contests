421b47c226e2b7f94153c7d932c499fd64a680fe
rheallyc <vireonxi@gmail.com>
2023-01-24 06:25:47 +0000

    Solve 312561B: Sequence Transformation

---
+++ solutions/312561B_Sequence_Transformation.cpp
@@ -0,0 +1,31 @@
+// Problem: Sequence Transformation
+// URL: https://codeforces.com/gym/312561/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+vector<int> ans;
+
+int main(){
+    scanf("%d", &n);
+    int sz=n, now=1;
+    while(sz){
+        if(sz==1){ ans.push_back(now); sz--;}
+        else if(sz==3){
+            ans.push_back(now);
+            ans.push_back(now);
+            ans.push_back(now*3);
+            sz-=3;
+        }
+        else{
+            int t=(sz+1)/2; sz-=t;
+            for(int i=1; i<=t; i++) ans.push_back(now);
+        }
+        now*=2;
+    }
+    for(auto x:ans) printf("%d ", x);
+    printf("\n");
+}
