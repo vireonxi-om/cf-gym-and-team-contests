10b4fce01fa9c069ea1bdcd5a20d4d3e9c5c648e
rheallyc <vireonxi@gmail.com>
2023-03-14 06:05:36 +0000

    Solve 302720D: Common Prefixes

---
+++ solutions/302720D_Common_Prefixes.cpp
@@ -0,0 +1,29 @@
+// Problem: Common Prefixes
+// URL: https://codeforces.com/gym/302720/problem/D
+// Rating: 1200
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+char a[60];
+int n, num;
+
+void solve(){
+    scanf("%d", &n);
+    printf("%s\n", a);
+    for(int i=0; i<n; i++){
+        scanf("%d", &num);
+        if(a[num]=='z') a[num]='a';
+        else a[num]++;
+        printf("%s\n", a);
+    }
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<60; i++) a[i]='a';
+    a[60]='\0';
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}
