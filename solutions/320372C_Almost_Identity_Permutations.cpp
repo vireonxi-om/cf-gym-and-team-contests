a2c7936c1ec10d91aa87330d1ab999ad497b92e6
rheallyc <vireonxi@gmail.com>
2023-03-16 16:01:54 +0000

    Solve 320372C: Almost Identity Permutations

---
+++ solutions/320372C_Almost_Identity_Permutations.cpp
@@ -0,0 +1,29 @@
+// Problem: Almost Identity Permutations
+// URL: https://codeforces.com/gym/320372/problem/C
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, k;
+
+int main(){
+    scanf("%d%d", &n, &k);
+    ll ans=0;
+    if(k==4){
+        ans+=(ll)3*n*(n-1)*(n-2)*(n-3)/8;
+        k--;
+    }
+    if(k==3){
+        ans+=(ll)n*(n-1)*(n-2)/3;
+        k--;
+    }
+    if(k==2){
+        ans+=(ll)n*(n-1)/2;
+        k--;
+    }
+    if(k==1) k--;
+    if(k==0) ans+=1;
+    printf("%lld\n", ans);
+}
