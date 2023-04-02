79af72bd445fa3c34cea654b7150d87c0ebce7ea
rheallyc <vireonxi@gmail.com>
2023-04-02 00:12:01 +0000

    Solve 303728D: Non-zero Segments

---
+++ solutions/303728D_Non-zero_Segments.cpp
@@ -0,0 +1,30 @@
+// Problem: Non-zero Segments
+// URL: https://codeforces.com/gym/303728/problem/D
+// Rating: 1500
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll sum;
+int n, result;
+
+int main(){
+    scanf("%d", &n);
+    map <ll, int> cnt;
+    sum = 0; cnt[0]++; result = 0;
+    ll num;
+    for(int i=0; i<n; i++){
+        scanf("%lld", &num);
+        sum+=num;
+        cnt[sum]++;
+        if (cnt[sum] == 2){
+            result++;
+            cnt.clear();
+            cnt[sum-num]++;
+            cnt[sum]++;
+        }
+    }
+    printf("%d\n", result);
+    return 0;
+}
