846b2814cff84bfa6b9a3e7265249277dd482fb2
rheallyc <vireonxi@gmail.com>
2023-04-15 06:14:36 +0000

    Solve 328370D: Arpa's loud Owf and Mehrdad's evil plan

---
+++ solutions/328370D_Arpas_loud_Owf_and_Mehrdads_evil_plan.cpp
@@ -0,0 +1,34 @@
+// Problem: Arpa's loud Owf and Mehrdad's evil plan
+// URL: https://codeforces.com/gym/328370/problem/D
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, a[110], yes[110];
+
+int gcd(int x, int y){
+    return y==0? x: gcd(y, x%y);
+}
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){ scanf("%d", &a[i]); yes[a[i]]=1;}
+    bool can=true;
+    for(int i=1; i<=n; i++) if(!yes[i]) can=false;
+    ll ans=1;
+    if(can){
+        for(int i=1; i<=n; i++){
+            int st=i, t=a[i], cnt=1;
+            while(t!=st){
+                t=a[t]; cnt++;
+            }
+            if(cnt%2) ans=(ll)ans*cnt/gcd(ans, cnt);
+            else ans=(ll)ans*cnt/gcd(ans, cnt/2)/2;
+        }
+        printf("%lld\n", ans);
+    }
+    else printf("-1\n");
+    return 0;
+}
