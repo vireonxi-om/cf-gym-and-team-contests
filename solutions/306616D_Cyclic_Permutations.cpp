af2f31df959fc82a8552d16bc443987bd6e0d233
rheallyc <vireonxi@gmail.com>
2023-04-21 23:22:04 +0000

    Solve 306616D: Cyclic Permutations

---
+++ solutions/306616D_Cyclic_Permutations.cpp
@@ -0,0 +1,25 @@
+// Problem: Cyclic Permutations 
+// URL: https://codeforces.com/gym/306616/problem/D
+// Rating: 1500
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define N 1000000007
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    ll a=1, b=1;
+    for(int i=1; i<=n; i++){
+        a*=i;
+        a=a%(N);
+    }
+    for(int i=1; i<=n-1; i++){
+        b*=2;
+        b=b%(N);
+    }
+    if(a<b) a+=N;
+    printf("%lld\n", (a-b)%(N));
+}
