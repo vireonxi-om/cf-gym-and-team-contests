f90dc1902050a6de1ff3ee2d56f22928f8b95294
rheallyc <vireonxi@gmail.com>
2023-05-09 15:22:36 +0000

    Solve 103202I: Rise of Shadows

---
+++ solutions/103202I_Rise_of_Shadows.cpp
@@ -0,0 +1,43 @@
+// Problem: Rise of Shadows
+// URL: https://codeforces.com/gym/103202/problem/I
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+
+LL h, m, a;
+int main()
+{
+	IO;
+	cin >> h >> m >> a;
+	LL d = __gcd(h - 1, h * m);
+	cout << min(h * m, d * (2 * (a / d) +1))  << "\n";
+	return 0;
+}
