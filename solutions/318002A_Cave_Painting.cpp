a04be54b06a3211198f762871f33f19c836ccdde
rheallyc <vireonxi@gmail.com>
2023-08-15 13:09:40 +0000

    Solve 318002A: Cave Painting

---
+++ solutions/318002A_Cave_Painting.cpp
@@ -0,0 +1,23 @@
+// Problem: Cave Painting
+// URL: https://codeforces.com/gym/318002/problem/A
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+ll n, k;
+
+
+int main(){
+    scanf("%lld%lld", &n, &k);
+    n++;
+    bool can=true;
+    for(ll i=k; i>=1; i--){
+        if(n%i){can=false; break;}
+    }
+    if(can) printf("Yes\n");
+    else printf("No\n");
+    return 0;
+}
+
