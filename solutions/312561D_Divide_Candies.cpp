c2d5803592b26ccf503269256fa120d32b25d89c
rheallyc <vireonxi@gmail.com>
2023-10-03 11:27:16 +0000

    Solve 312561D: Divide Candies

---
+++ solutions/312561D_Divide_Candies.cpp
@@ -0,0 +1,31 @@
+// Problem: Divide Candies
+// URL: https://codeforces.com/gym/312561/problem/D
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, m, num[1100];
+
+int main(){
+    scanf("%d%d", &n, &m);
+    for(int i=1; i<=m; i++){
+        int now=i, t=n/m;
+        if(now+t*m>n) num[(i*i)%m]+=t;
+        else num[(i*i)%m]+=t+1;
+    }
+    ll result=0;
+    result+=(ll)num[0]*num[0];
+    for(int i=1; i<=m/2; i++){
+        if(2*i!=m) result+=(ll)2*num[i]*num[m-i];
+        else result+=(ll)num[i]*num[i];
+    }
+    /*
+    for(int i=0; i<m; i++){
+        for(auto x:t[i]) printf("%d ", x);
+        printf("\n");
+    }*/
+    printf("%lld\n", result);
+}
+
