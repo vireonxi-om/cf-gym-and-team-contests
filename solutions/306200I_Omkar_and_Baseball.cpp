9c3e0e5dea1e8f9b4c2b0f41aa619616902995a8
rheallyc <vireonxi@gmail.com>
2023-10-07 20:47:03 +0000

    Solve 306200I: Omkar and Baseball

---
+++ solutions/306200I_Omkar_and_Baseball.cpp
@@ -0,0 +1,29 @@
+// Problem: Omkar and Baseball
+// URL: https://codeforces.com/gym/306200/problem/I
+// Rating: 1500
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[210000];
+
+void solve(){
+    scanf("%d", &n);
+    int cnt=0, cnt1=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        if(a[i]==i) cnt++;
+        if(i>1 && a[i]!=i && a[i-1]==i-1) cnt1++;
+    }
+    if(cnt==n) printf("0\n");
+    else if(a[1]==1 && cnt1>1 || a[1]!=1 && cnt1>0)
+        printf("2\n");
+    else printf("1\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}
