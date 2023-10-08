b564ffe7cf13786fd0830dcc5515beb66ac67e4f
rheallyc <vireonxi@gmail.com>
2023-10-08 13:22:57 +0000

    Solve 101291F: Equality

---
+++ solutions/101291F_Equality.cpp
@@ -0,0 +1,22 @@
+// Problem: Equality
+// URL: https://codeforces.com/gym/101291/problem/F
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a, b, c;
+
+int main(){
+    char ch;
+    for(int i=1; i<=9; i++){
+        scanf("%c", &ch);
+        if(i==1) a=ch-'0';
+        else if(i==5) b=ch-'0';
+        else if(i==9) c=ch-'0';
+    }
+    if(a+b==c) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
