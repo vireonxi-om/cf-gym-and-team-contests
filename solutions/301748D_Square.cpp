94fdbecdb23b36280d2dee6997a96f795bbe9800
rheallyc <vireonxi@gmail.com>
2023-10-21 16:06:10 +0000

    Solve 301748D: Square

---
+++ solutions/301748D_Square.cpp
@@ -0,0 +1,25 @@
+// Problem: Square
+// URL: https://codeforces.com/gym/301748/problem/D
+// Rating: 1200
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+long long n, k;
+
+void solve()
+{
+	scanf("%lld", &n);
+	if(((n+1)%4)==0) k=n+1;
+	else if(((n+1)%2)==0) k=2*n+1;
+	else k=4*n+1;
+	printf("%lld\n", k);
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	for(;t>0;t--) solve();
+	return 0;
+}
