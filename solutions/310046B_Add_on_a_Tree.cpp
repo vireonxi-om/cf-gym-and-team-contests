a8a027f7f207570db46c9d451efd29ec12873c2f
rheallyc <vireonxi@gmail.com>
2024-01-15 13:12:36 +0000

    Solve 310046B: Add on a Tree

---
+++ solutions/310046B_Add_on_a_Tree.cpp
@@ -0,0 +1,27 @@
+// Problem: Add on a Tree
+// URL: https://codeforces.com/gym/310046/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+vector<int> g[110000];
+
+int main(){
+    scanf("%d", &n);
+    for(int i=1; i<n; i++){
+        int u, v;
+        scanf("%d%d", &u, &v);
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    bool can=true;
+    for(int i=1; i<=n; i++){
+        if(g[i].size()==2) can=false;
+    }
+    if(can) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+}
