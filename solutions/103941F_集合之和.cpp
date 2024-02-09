1eee91860ce2acb7cfb3c9c6de1f5c60a3e64756
rheallyc <vireonxi@gmail.com>
2024-02-09 01:08:47 +0000

    Solve 103941F: 集合之和

---
+++ solutions/103941F_集合之和.cpp
@@ -0,0 +1,64 @@
+// Problem: 集合之和
+// URL: https://codeforces.com/gym/103941/problem/F
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 11000;
+const int M = 5100000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+
+
+
+int main() {
+    IO;
+    int n;
+    cin >> n;
+    bool yes = true;
+    if (n == 2 || n == 4) {
+        cout << "-1\n";
+    } 
+    else {
+        if (n % 2) {
+            int a = (n + 1) / 2;
+            cout << a << "\n";
+            repn(i, 1, a) cout << i << " ";
+            cout << "\n";
+        }
+        else {
+            int a = n / 2;
+            cout << a << "\n";
+            cout << "0 ";
+            repn(i, 2, a) cout << i << " ";
+            cout << "\n";
+            
+        }
+    }
+    return 0;
+}
+
+
+
