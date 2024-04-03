5cbbd2526281c8fef0e7285f1b36643756b81335
rheallyc <vireonxi@gmail.com>
2024-04-03 21:09:36 +0000

    Solve 103470M: Windblume Festival

---
+++ solutions/103470M_Windblume_Festival.cpp
@@ -0,0 +1,67 @@
+// Problem: Windblume Festival
+// URL: https://codeforces.com/gym/103470/problem/M
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 1100000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int T, n, a[N];
+LL ans, mn, mx;
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		ans = 0, mn = inf, mx = -inf;
+		repn(i, 1, n) {
+			cin >> a[i];
+			ans += abs(a[i]);
+			mn = min(mn, (LL)a[i]);
+			mx = max(mx, (LL)a[i]);
+		}
+		if (n == 1) {
+			cout << a[1] << "\n";
+			continue;
+		}
+		if (mn > 0) ans -= 2 * mn;
+		if (mx < 0) ans += 2 * mx;
+		cout << ans << "\n";
+	}
+	return 0;
+}
