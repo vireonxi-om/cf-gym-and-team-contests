2189a4dcf78c7253e58f73d13ab9f82a18e237f8
rheallyc <vireonxi@gmail.com>
2024-04-19 22:18:10 +0000

    Solve 310047C: Stas and the Queue at the Buffet

---
+++ solutions/310047C_Stas_and_the_Queue_at_the_Buffet.cpp
@@ -0,0 +1,28 @@
+// Problem: Stas and the Queue at the Buffet
+// URL: https://codeforces.com/gym/310047/problem/C
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, a[110000], b[110000];
+
+int main(){
+    scanf("%d", &n);
+    ll sum=0, sum2=0;
+    for(int i=1; i<=n; i++) {
+        scanf("%d%d", &a[i], &b[i]);
+        b[i] -= a[i];
+        sum += a[i];
+    }
+    sort(b+1, b+n+1);
+    for(int i=1; i<=n; i++){
+        sum2+=(ll)b[i]*(n-i);
+    }
+    sum*=(n-1);
+    sum+=sum2;
+    printf("%lld\n", sum);
+    return 0;
+}
+
