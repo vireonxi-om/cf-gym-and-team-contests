0c38c0982cdf47cbe4f362a62caff881e3928830
rheallyc <vireonxi@gmail.com>
2024-04-26 11:47:55 +0000

    Solve 314707B: Weakened Common Divisor

---
+++ solutions/314707B_Weakened_Common_Divisor.cpp
@@ -0,0 +1,43 @@
+// Problem: Weakened Common Divisor
+// URL: https://codeforces.com/gym/314707/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+set<int> ans;
+
+int main(){
+    scanf("%d", &n);
+    n--;
+    int a, b;
+    scanf("%d%d", &a, &b);
+    for(int i=2; i<=sqrt(a)+1; i++){
+        if(a%i==0){
+            ans.insert(i);
+            while(a%i==0) a/=i;
+        }
+    }
+    if(a>1) ans.insert(a);
+    for(int i=2; i<=sqrt(b)+1; i++){
+        if(b%i==0){
+            ans.insert(i);
+            while(b%i==0) b/=i;
+        }
+    }
+    if(b>1) ans.insert(b);
+    while(n--){
+        scanf("%d%d", &a, &b);
+        vector<int> temp;
+        for(auto x:ans){
+            if(a%x==0 || b%x==0) temp.push_back(x);
+        }
+        ans.clear();
+        for(auto x:temp) ans.insert(x);
+    }
+    if(ans.empty()) printf("-1\n");
+    else printf("%d\n", (*ans.begin()));
+    return 0;
+}
