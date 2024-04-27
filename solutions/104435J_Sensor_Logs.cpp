c71f0be2ae7bc72102707c18c022beb9cd0e5c61
rheallyc <vireonxi@gmail.com>
2024-04-27 17:55:01 +0000

    Solve 104435J: Sensor Logs

---
+++ solutions/104435J_Sensor_Logs.cpp
@@ -0,0 +1,63 @@
+// Problem: Sensor Logs
+// URL: https://codeforces.com/gym/104435/problem/J
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 9-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n, m, pp[N], last[N];
+int main() {
+	IO;
+	memset(last, -1, sizeof(last));
+	cin >> n >> m;
+	repn(i, 1, m) {
+		int x, y;
+		cin >> x >> y;
+		if (pp[x]) continue;
+		if (last[x] == -1) last[x] = y;
+		else {
+			if (last[x] != y) pp[x] = 1;
+			last[x] = -1;
+		}
+	}
+	rep(i, 0, n) cout << pp[i];
+	cout << "\n";
+	return 0;
+}
