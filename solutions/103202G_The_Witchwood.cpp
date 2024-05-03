5bb6d3af6bd258c7a136ec8f8c26d1c70d5301fb
rheallyc <vireonxi@gmail.com>
2024-05-03 02:54:43 +0000

    Solve 103202G: The Witchwood

---
+++ solutions/103202G_The_Witchwood.cpp
@@ -0,0 +1,47 @@
+// Problem: The Witchwood
+// URL: https://codeforces.com/gym/103202/problem/G
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 510000;
+const int M = 1100000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+const LL INF = 1e15;
+
+int n, k;
+LL a[1100], ans;
+
+
+int main(){
+    scanf("%d%d", &n, &k);
+    repn(i, 1, n) scanf("%lld", &a[i]);
+    sort(a + 1, a + n + 1);
+    pern(i, n - k + 1, n) ans += a[i];
+    printf("%lld\n", ans);
+    return 0;
+}
+
