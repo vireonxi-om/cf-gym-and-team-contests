4ba69db300cbfa63e77a81a6fc25af790531cbc0
rheallyc <vireonxi@gmail.com>
2024-05-04 00:14:40 +0000

    Solve 303728H: K-th Beautiful String

---
+++ solutions/303728H_K-th_Beautiful_String.cpp
@@ -0,0 +1,33 @@
+// Problem: K-th Beautiful String
+// URL: https://codeforces.com/gym/303728/problem/H
+// Rating: 1300
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+
+void solve(){
+    scanf("%d%d", &n, &k);
+    int i=1, j=k-1;
+    for(; ; i++){
+        j-=i;
+        if(j<0){
+            j+=i;
+            break;
+        }
+    }
+    for(int m=1; m<=n; m++){
+        if(m==n-j || m==n-i) printf("b");
+        else printf("a");
+    }
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}
+
