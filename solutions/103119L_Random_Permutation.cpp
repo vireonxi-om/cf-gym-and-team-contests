9d89808e45cb3fa5bd4fd2e6cbd40912b28bb892
rheallyc <vireonxi@gmail.com>
2024-06-08 00:11:49 +0000

    Solve 103119L: Random Permutation

---
+++ solutions/103119L_Random_Permutation.cpp
@@ -0,0 +1,47 @@
+// Problem: Random Permutation
+// URL: https://codeforces.com/gym/103119/problem/L
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const double eps = 1e-9;
+
+long double  ans = 1.0;
+int n;
+int main()
+{
+	IO;
+	cin >> n;
+	repn(i, 1, n) {
+		ans *= (long double)(n - i + 1) / (long double)n;
+		ans *= i;
+	}
+	printf("%.20f\n",(double)ans);
+	return 0;
+}
