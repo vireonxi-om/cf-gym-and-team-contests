52876b05cbfb208c58760ea7277be21c01fa1e47
rheallyc <vireonxi@gmail.com>
2024-06-19 17:25:12 +0000

    Solve 105143K: Party Games

---
+++ solutions/105143K_Party_Games.cpp
@@ -0,0 +1,62 @@
+// Problem: Party Games
+// URL: https://codeforces.com/gym/105143/problem/K
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++20 (GCC 13-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int T, n;
+int main() {
+    IO;
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        if (n % 4 == 3 || n % 4 == 2) {
+            cout << "Pinkie Pie\n";
+            continue;
+        }
+        if (n % 4 == 1 || n % 4 == 0) {
+            cout << "Fluttershy\n";
+            continue;
+        }
+    }
+    return 0;
+}
