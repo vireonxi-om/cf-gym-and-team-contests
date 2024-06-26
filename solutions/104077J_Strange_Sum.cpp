e24ee4322b54fc10d5c147faab4875c8fc86fdf4
rheallyc <vireonxi@gmail.com>
2024-06-26 11:26:31 +0000

    Solve 104077J: Strange Sum

---
+++ solutions/104077J_Strange_Sum.cpp
@@ -0,0 +1,55 @@
+// Problem: Strange Sum
+// URL: https://codeforces.com/gym/104077/problem/J
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 110000;
+const int M = 51000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n;
+LL a[N], m[N];
+
+
+int main() {
+	IO;
+	cin >> n;
+	repn(i, 1, n) cin >> a[i];
+	repn(i, 1, n) m[i] = max(a[i], m[i - 1]); 
+	LL ans = a[1];
+	pern(i, 2, n) {
+		ans = max(ans, a[i] + m[i - 1]);
+		ans = max(ans, a[i]);
+	}
+	ans = max(ans, (LL)0);
+	cout << ans << "\n";
+	return 0;
+}
+
+
+
