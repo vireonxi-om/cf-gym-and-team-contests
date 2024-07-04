740dba3a49390a8d23ca0a0ca6b8c91b7a4ad025
rheallyc <vireonxi@gmail.com>
2024-07-04 10:08:33 +0000

    Solve 301456E: Yet Another Bookshelf

---
+++ solutions/301456E_Yet_Another_Bookshelf.cpp
@@ -0,0 +1,38 @@
+// Problem: Yet Another Bookshelf
+// URL: https://codeforces.com/gym/301456/problem/E
+// Rating: 800
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[60];
+
+void solve(void)
+{
+	memset(a, 0, sizeof(a));
+	int count=0;
+	scanf("%d",&n); 
+	for(int i=0; i<n; i++){
+		scanf("%d", &a[i]);
+		if (a[i]==0) count++;
+	}
+	for(int i=0; ; i++){
+		if(a[i]==1) break;
+		else count--;
+	}
+	for(int i=n-1; ; i--){
+		if(a[i]==1) break;
+		else count--;
+	}
+	printf("%d\n", count);
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
