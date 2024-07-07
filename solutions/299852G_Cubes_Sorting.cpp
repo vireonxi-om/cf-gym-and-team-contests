b0c647f55fc25b6e31f32f766e3431093f1283e6
rheallyc <vireonxi@gmail.com>
2024-07-07 01:32:51 +0000

    Solve 299852G: Cubes Sorting

---
+++ solutions/299852G_Cubes_Sorting.cpp
@@ -0,0 +1,33 @@
+// Problem: Cubes Sorting
+// URL: https://codeforces.com/gym/299852/problem/G
+// Rating: 900
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <stdio.h>
+int a[51000];
+void solve(void)
+{
+    int n, result=0;
+    scanf("%d", &n);
+    for( int i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+        if(i>0 && a[i]>=a[i-1])
+            result=1;
+    }
+    if(result)
+        printf("YES\n");
+    else
+        printf("NO\n");
+}
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
+
