25d54406178d321ca4051cae0f02cebff334f1a0
rheallyc <vireonxi@gmail.com>
2024-07-08 18:16:43 +0000

    Solve 301748E: Maximum Number

---
+++ solutions/301748E_Maximum_Number.cpp
@@ -0,0 +1,19 @@
+// Problem: Maximum Number
+// URL: https://codeforces.com/gym/301748/problem/E
+// Rating: 1200
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, a[51000];
+
+int main()
+{
+	scanf("%d", &n);
+	int k=n/2;
+	for(int i=1; i<=k; i++) a[i]=1;
+	if(n%2==1) a[1]=7;
+	for(int i=1; i<=k; i++) printf("%d",a[i]);
+	printf("\n");
+	return 0;
+}
