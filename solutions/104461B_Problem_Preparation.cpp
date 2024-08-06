0a68b8c4d1e6a639b0830e09bb568e436ca350a5
rheallyc <vireonxi@gmail.com>
2024-08-06 06:23:15 +0000

    Solve 104461B: Problem Preparation

---
+++ solutions/104461B_Problem_Preparation.cpp
@@ -0,0 +1,67 @@
+// Problem: Problem Preparation
+// URL: https://codeforces.com/gym/104461/problem/B
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 9-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int T, n, a[N];
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		int mx = 0, n1 = 0, flag = 0;
+		repn(i, 1, n) cin >> a[i], mx = max(a[i], mx);
+		repn(i, 1, n) {
+			if (a[i] < 1) flag = 1;
+			if (a[i] == 1) n1++;
+		}
+		if (n < 10 || n > 13) flag = 1;
+		sort(a + 1, a + n + 1);
+		rep(i, 1, n) {
+			if (a[i] == mx || a[i + 1] == mx) continue;
+			if (abs(a[i] - a[i + 1]) > 2) flag = 1;
+		}
+		if (flag || n1 < 2) cout << "No\n";
+		else cout << "Yes\n";
+	}
+	return 0;
+}
