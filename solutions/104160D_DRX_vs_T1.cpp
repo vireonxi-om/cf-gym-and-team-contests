b038b7b8790da312885d79d20e47877eb4565247
rheallyc <vireonxi@gmail.com>
2024-08-14 16:29:53 +0000

    Solve 104160D: DRX vs. T1

---
+++ solutions/104160D_DRX_vs_T1.cpp
@@ -0,0 +1,54 @@
+// Problem: DRX vs. T1
+// URL: https://codeforces.com/gym/104160/problem/D
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 210000;
+const int M = 51000;
+const int mod = 998244353;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n; 
+
+int main() {
+	IO;
+	string s;
+	cin >> s;
+	int d = 0, t = 0;
+	rep(i, 0, 5) {
+		if (s[i] == 'T') t ++;
+		else d ++;
+		if (t >= 3 || d >= 3) break;
+		
+	}
+	if (t == 3) cout << "T1\n";
+	else cout << "DRX\n";
+	return 0;	
+}
+
+
+
