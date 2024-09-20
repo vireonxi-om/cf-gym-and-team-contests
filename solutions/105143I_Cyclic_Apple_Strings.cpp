f317e5553189e58e9a3ab8c21d713b2d37fd6168
rheallyc <vireonxi@gmail.com>
2024-09-20 14:24:34 +0000

    Solve 105143I: Cyclic Apple Strings

---
+++ solutions/105143I_Cyclic_Apple_Strings.cpp
@@ -0,0 +1,62 @@
+// Problem: Cyclic Apple Strings
+// URL: https://codeforces.com/gym/105143/problem/I
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++20 (GCC 13-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int n, n0, n1;
+string s;
+int main() {
+    IO;
+    cin >> s;
+    n = s.size();
+    if (s[0] == '0') n0++;
+    else n1++;
+    rep(i, 1, n) if (s[i] != s[i - 1]) {
+        if (s[i] == '0') n0++;
+        else n1++;
+    }
+    if (s[0] == '0') n0--;
+    if (s[n - 1] == '1') n1--;
+    cout << min(n0, n1) << "\n";
+    return 0;
+}
