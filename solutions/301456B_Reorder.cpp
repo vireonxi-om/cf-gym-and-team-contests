6c6ae7c27e7bd1d877acc490f83fdc0cdfd6c094
rheallyc <vireonxi@gmail.com>
2024-11-01 04:13:46 +0000

    Solve 301456B: Reorder

---
+++ solutions/301456B_Reorder.cpp
@@ -0,0 +1,33 @@
+// Problem: Reorder
+// URL: https://codeforces.com/gym/301456/problem/B
+// Rating: 800
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int m, n, result, a;
+
+void solve(void)
+{
+	result=0;
+	scanf("%d%d", &n, &m);
+	for(int i=1;i<=n;i++){
+		scanf("%d", &a);
+		result+=a;
+	} 
+	if(m==result)
+	printf("YES\n");
+	else
+	printf("NO\n");
+	
+	
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (;t>0;t--)
+        solve();
+    return 0;
+}
