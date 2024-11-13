ef3e4e24e7eccd86985c1bbd4aa8ee6a94f3838c
rheallyc <vireonxi@gmail.com>
2024-11-13 01:53:44 +0000

    Solve 104901D: Largest Digit

---
+++ solutions/104901D_Largest_Digit.cpp
@@ -0,0 +1,68 @@
+// Problem: Largest Digit
+// URL: https://codeforces.com/gym/104901/problem/D
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+int T, a, b, c, d;
+int cal(int x) {
+	int res = 0;
+	while (x) res = max(res, x % 10), x /= 10;
+	return res;
+}
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> a >> b >> c >> d;
+		int ans = 0;
+		repn(i, a, b) {
+			repn(j, c, d) {
+				ans = max(ans, cal(i + j));
+				if (ans == 9) break;
+			}
+			if (ans == 9) break;
+		}
+		cout << ans << "\n";
+	}
+	return 0;
+}
