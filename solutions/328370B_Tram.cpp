273b4485b82c929669a669e9ecac8a09dd07e364
rheallyc <vireonxi@gmail.com>
2024-12-24 03:46:13 +0000

    Solve 328370B: Tram

---
+++ solutions/328370B_Tram.cpp
@@ -0,0 +1,31 @@
+// Problem: Tram
+// URL: https://codeforces.com/gym/328370/problem/B
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int s, p, d, t1, t2, st, t;
+
+int cal(int x1, int a){
+    if(x1>a) return 2*s-x1-a;
+    else if(x1>-a) return a-x1;
+    else return -a-x1;
+}
+
+int main(){
+    scanf("%d%d%d%d%d", &s, &p, &d, &t1, &t2);
+    scanf("%d%d", &st, &t);
+    st*=t;
+    int ans;
+    if(st>p){
+        ans=cal(st, p)+cal(-p, d);
+    }
+    else if(st>-p) ans=cal(st, p)+cal(p, d);
+    else ans=cal(st, p)+cal(-p, d);
+    ans*=t1;
+    ans=min(ans, t2*abs(p-d));
+    printf("%d\n", ans);
+    return 0;
+}
