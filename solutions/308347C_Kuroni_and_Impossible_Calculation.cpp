d7115a3161b6802e0a1c36fdfc22d0831ee3d275
rheallyc <vireonxi@gmail.com>
2025-04-12 15:56:07 +0000

    Solve 308347C: Kuroni and Impossible Calculation

---
+++ solutions/308347C_Kuroni_and_Impossible_Calculation.cpp
@@ -0,0 +1,31 @@
+// Problem: Kuroni and Impossible Calculation
+// URL: https://codeforces.com/gym/308347/problem/C
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n, m, result;
+int a[210000], dif[210000];
+
+int main(){
+    scanf("%d%d", &n, &m);
+    for(int i=1; i<=n; i++) scanf("%d", &a[i]);
+    sort(a+1, a+n+1);
+    for(int i=1; i<=n; i++) dif[i]=a[i]-a[1];
+    if(n>m) result=0;
+    else{
+        result=1;
+        for(int i=1; i<n; i++){
+            for(int j=i+1; j<=n; j++){
+                result*=((dif[j]-dif[i])%m);
+                result=result%m;
+            }
+        }
+    }
+    printf("%d\n", result);
+
+    return 0;
+}
+
