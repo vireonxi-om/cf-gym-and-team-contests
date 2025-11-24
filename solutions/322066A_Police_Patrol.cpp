2f208c35e7c91c205143661c353275146f7aeb03
rheallyc <vireonxi@gmail.com>
2025-11-24 05:37:09 +0000

    Solve 322066A: Police Patrol

---
+++ solutions/322066A_Police_Patrol.cpp
@@ -0,0 +1,18 @@
+// Problem: Police Patrol
+// URL: https://codeforces.com/gym/322066/problem/A
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, ans;
+
+int main(){
+    scanf("%d%d", &n, &k);
+    int t=n/(k+1);
+    ans=t*2;
+    int lft=n-t*(k+1);
+    if(lft>1) ans+=2;
+    else if(lft==1) ans++;
+    printf("%d\n", ans);
+}
