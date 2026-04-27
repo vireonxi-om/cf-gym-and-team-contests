0f1f8015977b704465759bd1325fe13e98188271
rheallyc <vireonxi@gmail.com>
2026-04-27 11:09:48 +0000

    Solve 328644A: Memory and De-Evolution

---
+++ solutions/328644A_Memory_and_De-Evolution.cpp
@@ -0,0 +1,24 @@
+// Problem: Memory and De-Evolution
+// URL: https://codeforces.com/gym/328644/problem/A
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int x, y;
+
+int main(){
+    scanf("%d%d", &x, &y);
+    int m1=y, m2=y, m3=y, cnt=0;
+    while(1){
+        m1=m2;
+        int t=m3;
+        m3=m3+m2-1;
+        m2=t;
+        cnt++;
+        if(m3>=x){ cnt+=2; break;}
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
