92fc3dad4b4c7ece3c097dffe1b21a1061e10d7f
rheallyc <vireonxi@gmail.com>
2026-04-30 16:20:54 +0000

    Solve 308172D: Circle of Monsters

---
+++ solutions/308172D_Circle_of_Monsters.cpp
@@ -0,0 +1,34 @@
+// Problem: Circle of Monsters
+// URL: https://codeforces.com/gym/308172/problem/D
+// Rating: 1600
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+ll a[310000], b[310000], c[310000];
+
+void solve(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        scanf("%lld%lld", &a[i], &b[i]);
+    }
+    ll sum=0;
+    for(int i=2; i<=n; i++){
+        c[i]=max((ll)0, a[i]-b[i-1]);
+        sum+=c[i];
+    }
+    c[1]=max((ll)0, a[1]-b[n]); sum+=c[1];
+    ll result=1e18;
+    for(int i=1; i<=n; i++) result=min(result, a[i]-c[i]+sum);
+    printf("%lld\n", result);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=1; i<=t; i++) solve();
+    return 0;
+}
+
