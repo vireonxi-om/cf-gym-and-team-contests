d60088eafbf547a5450e33c358ec71d08a13dea5
rheallyc <vireonxi@gmail.com>
2026-05-05 08:47:25 +0000

    Solve 301748B: Mischievous Mess Makers

---
+++ solutions/301748B_Mischievous_Mess_Makers.cpp
@@ -0,0 +1,22 @@
+// Problem: Mischievous Mess Makers
+// URL: https://codeforces.com/gym/301748/problem/B
+// Rating: 1200
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+long long result;
+
+int main()
+{
+	scanf("%d%d", &n, &k);
+	if(n==1) result=0;
+	else if(k<(n+1)/2){
+		result=(long long)(n-1)*k + (n-2*k)*k;
+	}
+	else
+		for(int i=1; i<n; i++) result+=i;
+	printf("%lld\n",result);
+	return 0;
+}
