2431e8117881fc384a5b800f2069ff11f55f5e06
rheallyc <vireonxi@gmail.com>
2026-08-01 16:27:39 +0000

    Solve 471068F:

---
+++ solutions/471068F_.cpp
@@ -0,0 +1,162 @@
+// Problem: 
+// URL: https://codeforces.com/gym/471068/problem/F
+// Rating: unrated
+// Tags: none (gym/team contest)
+// Language: C++17 (GCC 9-64)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define repn(i, a, n) for (int i = a; i <= n; i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i, a, n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<LL, int> PLI;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+mt19937_64 Rand((unsigned long long)new char);
+#define rand Rand
+
+typedef long double DOUBLE;
+typedef vector<DOUBLE> VD;
+typedef vector<VD> VVD;
+typedef vector<int> VI;
+ 
+const DOUBLE EPS = 1e-9;
+struct LPSolver {
+    int m, n;
+    VI B, N;
+    VVD D;
+    LPSolver(const VVD &A, const VD &b, const VD &c) :
+        m(b.size()), n(c.size()), N(n + 1), B(m), D(m + 2, VD(n + 2)) {
+        for (int i = 0; i < m; i++) for (int j = 0; j < n; j++) D[i][j] = A[i][j];
+        for (int i = 0; i < m; i++) { B[i] = n + i; D[i][n] = -1; D[i][n + 1] = b[i]; }
+        for (int j = 0; j < n; j++) { N[j] = j; D[m][j] = -c[j]; }
+        N[n] = -1; D[m + 1][n] = 1;
+    }
+    void Pivot(int r, int s) {
+        for (int i = 0; i < m + 2; i++) if (i != r)
+            for (int j = 0; j < n + 2; j++) if (j != s)
+                D[i][j] -= D[r][j] * D[i][s] / D[r][s];
+        for (int j = 0; j < n + 2; j++) if (j != s) D[r][j] /= D[r][s];
+        for (int i = 0; i < m + 2; i++) if (i != r) D[i][s] /= -D[r][s];
+        D[r][s] = 1.0 / D[r][s];
+        swap(B[r], N[s]);
+    }
+    bool Simplex(int phase) {
+        int x = phase == 1 ? m + 1 : m;
+        while (true) {
+            int s = -1;
+            for (int j = 0; j <= n; j++) {
+                if (phase == 2 && N[j] == -1) continue;
+                if (s == -1 || D[x][j] < D[x][s] || D[x][j] == D[x][s] && N[j] < N[s]) s = j;
+            }
+            if (D[x][s] > -EPS) return true;
+            int r = -1;
+            for (int i = 0; i < m; i++) {
+                if (D[i][s] < EPS) continue;
+                if (r == -1 || D[i][n + 1] / D[i][s] < D[r][n + 1] / D[r][s] ||
+                    (D[i][n + 1] / D[i][s]) == (D[r][n + 1] / D[r][s]) && B[i] < B[r]) r = i;
+            }
+            if (r == -1) return false;
+            Pivot(r, s);
+        }
+    }
+    DOUBLE Solve(VD &x) {
+        int r = 0;
+        for (int i = 1; i < m; i++) if (D[i][n + 1] < D[r][n + 1]) r = i;
+        if (D[r][n + 1] < -EPS) {
+            Pivot(r, n);
+            if (!Simplex(1) || D[m + 1][n + 1] < -EPS) return -numeric_limits<DOUBLE>::infinity();
+            for (int i = 0; i < m; i++) if (B[i] == -1) {
+                int s = -1;
+                for (int j = 0; j <= n; j++)
+                    if (s == -1 || D[i][j] < D[i][s] || D[i][j] == D[i][s] && N[j] < N[s]) s = j;
+                Pivot(i, s);
+            }
+        }
+        if (!Simplex(2)) return numeric_limits<DOUBLE>::infinity();
+        x = VD(n);
+        for (int i = 0; i < m; i++) if (B[i] < n) x[B[i]] = D[i][n + 1];
+        return D[m][n + 1];
+    }
+};
+int T, n, m, a[N], cas, cnt;
+VVD A;
+VD B, C, x;
+int f[1100][11], b[N];
+void dfs(int depth, int num) {
+	if (depth == n + 1) {
+		repn(i, 1, n) f[cnt][i] = b[i];
+		cnt++;
+		return;
+	}
+	if (num + a[depth] <= m) b[depth] = 1, dfs(depth + 1, num + a[depth]);
+	b[depth] = 0, dfs(depth +1, num);
+}
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> n >> m;
+		repn(i, 1, n) cin >> a[i];
+		A.clear(), B.clear(), C.clear();
+		cnt = 0;
+		dfs(1, 0);
+		double ans;
+		if (cnt == 0) ans = 0;
+		else {
+			repn(i, 1, n) {
+				VD res;
+				rep(j, 0, cnt) {
+					if (f[j][i]) res.pb(1);
+					else res.pb(0);
+				}
+				res.pb(-1);
+				A.pb(res), B.pb(0);
+				res.clear();
+				rep(j, 0, cnt) {
+					if (f[j][i]) res.pb(-1);
+					else res.pb(0);
+				}
+				res.pb(1);
+				A.pb(res), B.pb(0);
+			}
+			VD res;
+			rep(i, 0, cnt) res.pb(1);
+			res.pb(0);
+			A.pb(res), B.pb(1);
+			rep(i, 0, cnt) C.pb(0);
+			C.pb(1);
+			LPSolver solver(A, B, C);
+			ans = solver.Solve(x);
+		}
+		printf("Case #%d: %.15f\n", ++cas, ans);
+	}
+	return 0;
+}
